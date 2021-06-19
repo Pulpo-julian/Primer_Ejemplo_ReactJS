54243ec84ab9de6d14b56baf566375de667347de
julian-pulpo <julian.schrodinger@gmail.com>
2021-06-18 23:59:49 -0500

    declaro un nuevo componente

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
 
+function Helloworld() {
+  return (
+    <div id="hola">Hola todo bien??</div>
+  )
+}
+
 function App() {
   return (
-    <div>Hello World!!</div>
+    <div>este sera mi primer export en react: <Helloworld/> </div>
   );
 }
 
